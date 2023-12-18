e23ddeaf0a7492ee26ff6c275b6f2b9c3d761f4f
cathlauretta <18221157@std.stei.itb.ac.id>
2023-12-18 13:09:53 +0700

    feat: progress FE

--- app/Views/styling.php
+++ app/Views/styling.php
@@ -4,33 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commercial System</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/card.css'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/med-info.css'); ?>">
     <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
 </head>
 <style>
     body {
         font-family: 'Inter';
+        width: 90%;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
     }
 </style>
 
-<body>
-    <article class="med-review">
-        <img class="image-2 image" src="../image-2.png" alt="Med Image"/>
-        <div class="frame-37">
-            <div class="brand-name valign-text-middle body-regular-1">
+<body> 
+    <article class="med-review-card">
+        <img class="image-1" src="../image-2.png" alt="Med Image"/>
+        <div class="info-container">
+            <div class="brand-name body-regular-1">
                 Aldactone
             </div>
-            <div class="generic-name valign-text-middle body-semibold-1">
+            <div class="generic-name body-semibold-1">
                 Spironolactone
             </div>
-            <div class="frame-42">
-                <div class="button">
-                    <div class="text valign-text-middle button1">
+            <div class="button-wrapper">
+                <div class="align-text-middle button1">
                         Add Review
-                    </div>
                 </div>
             </div>
         </div>
     </article>
+
+    <div class="med-info-container">
+        <div class="headline">
+            <img class="image-2" src="../image-2.png" alt="Med Image"/>
+            <div class="text-1 text-3">
+                <div class="med-name align-paragraph-middle headline-semibold-4">
+                    Aldactone Spironolactone
+                </div>
+                <div class="review">
+                    <div class="percent align-paragraph-middle headline-semibold-2">86%</div>
+                    <div class="review-item align-paragraph-middle body-regular-2">Recommended</div>
+                    <div class="review-item align-paragraph-middle boddy-regular-2">(13 of 15)</div>
+                </div>
+            </div>
+        </div>
+        <div class="med">
+            <div class="tion align-text-middle body-semibold-2">Brief Explanation</div>
+            <p class="tion align-text-middle body-regular-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet purus gravida quis blandit turpis cursus. Congue nisi vitae suscipit tellus mauris a diam.
+            </p>
+        </div>
+        <div class="med">
+            <div class="tion align-text-middle body-semibold-2">Composition</div>
+            <p class="tion align-text-middle body-regular-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet purus gravida quis blandit turpis cursus. Congue nisi vitae suscipit tellus mauris a diam.
+            </p>
+        </div>
+    </div>
 </body>
 </html>
